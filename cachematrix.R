d7d844549319f8f1403d00c5ea4e7ef467669f34
Nour Qweder <nour.qweder@gmail.com>
2019-10-22 12:50:04 +0200

    updated

--- cachematrix.R
+++ cachematrix.R
@@ -1,34 +1,82 @@
-## This script will cache the inverse of a matrix.
+## Put comments here that give an overall description of what your
+## functions do
 
-## This function will create a list of functions to set a matrix and its inverse
+## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+
+
+makeVector <- function(x = numeric()) {
         m <- NULL
         set <- function(y) {
                 x <<- y
                 m <<- NULL
         }
         get <- function() x
-        setinverse <- function(solve) m <<- solve
-        getinverse <- function() m
+        setmean <- function(mean) m <<- mean
+        getmean <- function() m
         list(set = set, get = get,
-             setinverse = setinverse,
-             getinverse = getinverse)
+             setmean = setmean,
+             getmean = getmean)
+}
+
+m <- function(x = matrix()) {
+    #1 Initialize cached inverse matrix
+    inv <- NULL
+     #flag to check if matrix has updated or not
+    updated <- TRUE
+    
+    # Set a matrix in the data structure
+    set_m <- function(x) {
+        m <<- x
+        #init status for matrix
+        mat_inv <<- NULL
+        #flag to check if matrix has updated or not
+        updated <<- TRUE
+    }
+    
+    # Get the currently set matrix
+    get_m <- function() m
+    
+    # Set calculate the inverse matrix, 
+    # if matrix has not updated -> false
+    set_inverse_matrix <- function(inverse) {
+        updated <<- FALSE
+        mat_inv <<- inverse
+    }
+
+
+## cache inverse m
+ #  inverse matrix
+    get_inverse_matrix <- function() inv
+    
+    #detected updated 
+    is_updated <- function() updated
+    #all functions
+    list(set_m = set_m, 
+         get_m = get_m,
+         set_inverse_matrix = set_inverse_matrix,
+         get_inverse_matrix = inverse_matrix,
+         is_updated = is_updated)
 }
 
 
-## This function solves for a matrix's inverse if it hasn't been cached previously.
+## cacheSolve function calculate the inverse "matrix" returned by m from previous func
+#if inverse is available in chch but matrix has not upddated so it retrived from cach
 
-cacheSolve <- function(x, ...) {
-        m <- x$getinverse()
-        if(!is.null(m)) {
-                message("getting cached data")
-                return(m)
-        }
-        data <- x$get()
-        m <- solve(data, ...)
-        x$setinverse(m)
-        m
+cacheSolve <- function(my_matrix, ...) {
+    # cached inverse matrix
+    inv <- my_matrix$get_inverse_matrix()
+    #check if matrix has been updated 
+
+    updated <- my_matrix$is_updated()
+    
+    m <- my_matrix$get_m()
+    
+    # Calculate the inverse of matrix
+    inv <- solve(m, ...)
+    # Set the calculated matrix that's inversed
+    my_matrix$set_inverse_matrix(inv)
+    
+    message(" inverse of matrix is DONE!")
+    inv
 }
-        
-
